1c720e9dedb6f3e380f86dad4d9eae9edda6f25c
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-09-22 15:36:29 +0200

    carousel bootstrap ok mais sans photo

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -29,10 +29,53 @@ class MyNav extends HTMLElement {
     `;
   }
 }
+class MyFooter extends HTMLElement {
+  connectedCallback() {
+      this.innerHTML = `
+      <div class="container">
+          <footer class="py-3 mt-4 mb-2">
+              <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1 footerLogo">
+                  <img src="/static/assets/picto_rose.png" alt="logo chatbot footer" width="30" height="24" class="rounded">
+              </a>
+              <span class="mb-3 mb-md-0">©2023 LoveRoom13 - Tous droits réservés.</span>               
+              <ul class="mb-3 mb-md-0">
+                  <li><a class=" href="#"><i class="fa-brands fa-snapchat"></i></a></li>
+                  <li><a class=" href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+              </ul>
+          </footer>
+      </div>
+      `;
+  }
+}
+class MyCarousel extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+`;
+}
+}
 
 // C'est cette ligne qui fait la magie. Elle indique au navigateur comment utiliser <my-nav> comme un élément réel.
 customElements.define('my-nav', MyNav);
-
-// Si vous avez également un composant 'my-modal', assurez-vous de le définir de la même manière.
-// Par exemple:
-// customElements.define('my-modal', MyModal);
+customElements.define('my-carousel', MyCarousel);
+customElements.define('my-footer', MyFooter);
