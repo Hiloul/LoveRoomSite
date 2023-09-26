935db6369a3710b982254fc953d9172bbbb4777a
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-09-26 15:07:24 +0200

    nav

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -3,15 +3,12 @@ class MyNav extends HTMLElement {
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#home"><img src="../assets/heartkey.png"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#home">Accueil <span class="sr-only">(current)</span></a>
-              </li>
               <li class="nav-item">
                 <a class="nav-link" href="#pricing">Tarifs</a>
               </li>
