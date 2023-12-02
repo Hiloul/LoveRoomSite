01faba31546c5ba43dc95133081b55e98f02e42c
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-12-02 22:49:59 +0100

    script commenté

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+{/* <script>
+document.addEventListener('DOMContentLoaded', function () {
+  const snapchatLi = document.getElementById('snapchat');
+  const whatsappLi = document.getElementById('whatsapp');
+
+  snapchatLi.addEventListener('click', function () {
+    this.classList.toggle('active');
+    whatsappLi.classList.remove('active');
+  });
+
+  whatsappLi.addEventListener('click', function () {
+    this.classList.toggle('active');
+    snapchatLi.classList.remove('active');
+  });
+});
+</script> */}
+// Script au dessu pour regler le probleme du hover sur un ecran de telephone
