fdb2d8671a243f1228cb2431aff5bd747f5ee832
Hilel Hamidi <hilel.hamidi@gmail.com>
2023-12-02 23:01:29 +0100

    carousel ok manque photo

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -48,13 +48,13 @@ class MyCarousel extends HTMLElement {
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="../assets/logotest.png" alt="First slide">
+      <img class="d-block w-100 h-50" src="../assets/logotest.png" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="../assets/logotest.png" alt="Second slide">
+      <img class="d-block w-100 h-50" src="../assets/logotest.png" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="../assets/logotest.png" alt="Third slide">
+      <img class="d-block w-100 h-50" src="../assets/logotest.png" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
