463566bfb46726a2ecdc72a6598f2f0a77000e6a
Hilel Hamidi <hilel.hamidi@gmail.com>
2024-05-22 19:05:41 +0200

    changement de photo

--- js/webcomponents.js
+++ js/webcomponents.js
@@ -47,13 +47,13 @@ class MyCarousel extends HTMLElement {
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100 h-50" src="./assets/fall.jpg" alt="First slide">
+      <img class="d-block w-100 h-50" src="./assets/jacuzzi.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100 h-50" src="./assets/alley.jpg" alt="Second slide">
+      <img class="d-block w-100 h-50" src="./assets/loft.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100 h-50" src="./assets/nature.jpg" alt="Third slide">
+      <img class="d-block w-100 h-50" src="./assets/room.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
